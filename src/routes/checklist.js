278b76765a04082ec41c9e7b55a5a18142ac2055
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2021-01-22 18:21:23 -0300

    CRUD with checklist

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -1,19 +1,68 @@
 import express from 'express'
+import Checklist from '../models/checklist.js'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    console.log('Ola')
-    res.send()
+router.get('/', async (req, res) => {
+    try {
+        const checklists = await Checklist.find({})
+
+        res.status(200).json(checklists)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
-router.get('/:id', (req, res) => {
-    console.log(req.params.id)
-    res.send(`ID: ${req.params.id}`)
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const checklist = await Checklist.findById(id)
+
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
 })
 
-router.post('/', (req, res) => {
-    res.status(200).json(req.body)
+router.put('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const { name } = req.body
+
+        const checklist = await Checklist.findByIdAndUpdate(
+            id,
+            { name },
+            { new: true }
+        )
+
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const checklist = await Checklist.findByIdAndRemove(id)
+
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
+})
+
+router.post('/', async (req, res) => {
+    const { name } = req.body
+
+    try {
+        const checklist = await Checklist.create({ name })
+
+        res.status(200).json(checklist)
+    } catch (error) {
+        res.status(422).json(error)
+    }
 })
 
 export default router
