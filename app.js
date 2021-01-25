95cad9706aefbac38ea1fa016bc2ace33da28dd3
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2021-01-25 13:44:47 -0300

    Configurado EJS

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 import express from 'express'
+import path from 'path'
 import CheckListRouter from './src/routes/checklist.js'
+import RootRouter from './src/routes/index.js'
+
 import './config/database.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.set('views', path.join(path.resolve(), 'src/views'))
+app.set('view engine', 'ejs')
+
+app.use('/', RootRouter)
 app.use('/checklists', CheckListRouter)
 
 app.listen(3333, () => console.log('server started'))
