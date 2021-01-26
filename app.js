fa27bce6f2053561feac623db703a05ecd41263d
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2021-01-26 14:23:24 -0300

    Adicionado e configurado biblioteca method-override

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import path from 'path'
+import MethodOverride from 'method-override'
 import CheckListRouter from './src/routes/checklist.js'
 import RootRouter from './src/routes/index.js'
 
@@ -9,6 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(MethodOverride('_method'))
 
 app.set('views', path.join(path.resolve(), 'src/views'))
 app.set('view engine', 'ejs')
