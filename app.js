7e1b6e15a4fcfd2810e858106fa1e73e664ef369
Bruno Yuri Jacinto <bruno.orle@gmail.com>
2021-01-26 14:00:07 -0300

    Criado uma tarefa

--- app.js
+++ app.js
@@ -8,9 +8,12 @@ import './config/database.js'
 const app = express()
 
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.set('views', path.join(path.resolve(), 'src/views'))
 app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(path.resolve(), 'public')))
 
 app.use('/', RootRouter)
 app.use('/checklists', CheckListRouter)
--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -7,9 +7,19 @@ router.get('/', async (req, res) => {
     try {
         const checklists = await Checklist.find({})
 
-        res.status(200).json(checklists)
+        res.status(200).render('checklists/index', { checklists })
     } catch (error) {
-        res.status(500).json(error)
+        res.status(500).render('pages/error', { error })
+    }
+})
+
+router.get('/new', async (req, res) => {
+    try {
+        const checklist = new Checklist()
+
+        res.status(200).render('checklists/new', { checklist })
+    } catch (error) {
+        res.status(422).render('pages/error', { error })
     }
 })
 
@@ -18,9 +28,9 @@ router.get('/:id', async (req, res) => {
         const { id } = req.params
         const checklist = await Checklist.findById(id)
 
-        res.status(200).json(checklist)
+        res.status(200).render('checklists/show', { checklist })
     } catch (error) {
-        res.status(422).json(error)
+        res.status(422).render('pages/error', { error })
     }
 })
 
@@ -54,14 +64,16 @@ router.delete('/:id', async (req, res) => {
 })
 
 router.post('/', async (req, res) => {
-    const { name } = req.body
+    const { name } = req.body.checklist
 
     try {
-        const checklist = await Checklist.create({ name })
+        await Checklist.create({ name })
 
-        res.status(200).json(checklist)
+        res.redirect('/checklists')
     } catch (error) {
-        res.status(422).json(error)
+        res.status(422).render('checklists/new', {
+            checklist: { ...req.body.checklist, error },
+        })
     }
 })
 
